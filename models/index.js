46e0a4da5cfadb1283a36781a96ab5f9b508a57f
jmichaeldavis <jmichaeldavis17@gmail.com>
2023-05-30 18:51:28 -0600

    added more to the models and seeds

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Reminder = require('./Reminder');
 
-module.exports = { User };
+User.hasMany(Reminder, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Reminder.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Reminder };
